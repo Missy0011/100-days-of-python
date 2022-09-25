160253a95ae02b7484b8e2d50ed0b5aaf2e746f1
Moumita Basak <93520679+Missy0011@users.noreply.github.com>
2022-09-25 19:39:12 +0530

    Add files via upload

---
+++ Day 1.py
@@ -0,0 +1,21 @@
+print("Hello world \n hello world")  # \n for next line
+print("Hello"+" " + "Moumita")
+# when we have to use some "" then we will use '' instead of ""
+print('I want to say,"I love you".')
+
+
+a = 10
+b = 20
+# print("The value of a is"a"the value of b is"b")
+# f & {a} makes easier to print the value of a and b
+print(f"value of a = {a} & value of b = {b}")
+#print("hello "+input("enter your name"))
+#print(len(input("What's your name?")))
+a=("moumi hh")
+print(len(a))
+#brand name generator
+city=input("Where did you born?")
+pet=input("What's the name of your pet?")
+print("your brand name is "+city+" "+pet)
+
+
